60f5f2a8572cc4352b12fec969af29f01ff341c3
Вова Петров <wolfram@macbook-pro-vova.local>
2018-10-17 00:02:47 +0500

    1

--- TestExample4Tests/TestExample4Tests.swift
+++ TestExample4Tests/TestExample4Tests.swift
@@ -19,8 +19,39 @@ class TestExample4Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testRegistration() {
-        XCTAssertEqual(<#T##expression1: Equatable##Equatable#>, <#T##expression2: Equatable##Equatable#>)
+    func testAdd() {
+        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "add") as! AddViewController
+        UIWindow.
+        vc.expSwitch.isOn = true
+        vc.nameField.text = "Vova"
+        vc.postField.text = "Professor"
+        vc.wageField.text = "5423"
+        vc.startDatePicker.date = Date.init()
+        XCTAssertEqual(true, vc.saveCheck())
+        vc.nameField.text = ""
+        XCTAssertEqual(false, vc.saveCheck())
+        vc.nameField.text = "Vova"
+        vc.postField.text = ""
+        XCTAssertEqual(false, vc.saveCheck())
+        vc.postField.text = "Professor"
+        vc.wageField.text = ""
+        XCTAssertEqual(false, vc.saveCheck())
+    }
+    
+    func testChange() {
+        let vc = ChangeViewController()
+        vc.searchField.text = "Vov"
+        XCTAssertEqual(false, vc.searchCheck())
+        vc.searchField.text = "Vova"
+        XCTAssertEqual(true, vc.searchCheck())
+        vc.postField.text = "Professor Number One"
+        XCTAssertEqual(true, vc.saveCheck())
+        XCTAssertEqual(true, vc.searchCheck())
+        XCTAssertEqual(vc.postField.placeholder, "Professor Number One")
+        vc.wageField.text = "145"
+        XCTAssertEqual(true, vc.saveCheck())
+        XCTAssertEqual(true, vc.searchCheck())
+        XCTAssertEqual(vc.wageField.placeholder, "145")
     }
 
     func testPerformanceExample() {
