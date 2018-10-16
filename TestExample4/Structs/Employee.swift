c2ac2c4111dec1782af5ae67228d48acde9da18d
Варя <varya@macbook-air.local>
2018-10-16 18:49:25 +0500

    Add structure

---
+++ TestExample4/Structs/Employee.swift
@@ -0,0 +1,18 @@
+//
+//  Employee.swift
+//  TestExample4
+//
+//  Created by варя on 16/10/2018.
+//  Copyright © 2018 Вова Петров. All rights reserved.
+//
+
+import Foundation
+
+struct Employee {
+    var name: String = ""
+    var post: String = ""
+    var status = false
+    var wage: UInt = 0
+    var isHaveExperience = false
+    var startDate = Date()
+}
---
+++ TestExample4/UserTableController.swift
@@ -0,0 +1,9 @@
+//
+//  UserTableController.swift
+//  TestExample4
+//
+//  Created by варя on 16/10/2018.
+//  Copyright © 2018 Вова Петров. All rights reserved.
+//
+
+import Foundation
